b2ed024d2317e57d13c323c61f3063830ace5ccb
firstlove <c941968215@gmail.com>
2019-03-02 19:11:15 +0800

    translation done

---
+++ translation/.auctex-auto/Chinese.el
@@ -0,0 +1,46 @@
+(TeX-add-style-hook
+ "Chinese"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("ulem" "normalem") ("ctex" "UTF8")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "float"
+    "ctex")
+   (LaTeX-add-labels
+    "sec:orgc686a7c"
+    "sec:orga9c576e"
+    "sec:org6aa854c"
+    "sec:org4ae9519"
+    "sec:org3009fa0"
+    "sec:org4e54e98"
+    "sec:org0755e36"
+    "sec:org6f134f6"
+    "sec:orgfec6faf"
+    "sec:org1424aba")
+   (LaTeX-add-bibliographies
+    "man"))
+ :latex)
+
