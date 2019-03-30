b731d63964c743d77d14392b639d7c34b3818719
firstlove <c941968215@gmail.com>
2019-03-30 09:00:04 +0800

    misc

--- .auctex-auto/manuscript.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook
- "manuscript"
- (lambda ()
-   (LaTeX-add-bibitems
-    "Kusuma_Wardhani_2017"
-    "sitorus13_biogas_recov_from_anaer_diges"))
- :bibtex)
-
--- .auctex-auto/papermaking.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "papermaking"
- (lambda ()
-   (LaTeX-add-bibitems
-    "造纸白泥制备陶瓷滤料的技术研究"))
- :bibtex)
-
